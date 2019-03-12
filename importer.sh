b5234a3baae67d95756ef978f1a39bb0013f405b
sandweel <sysadmin@aws3.com>
2019-03-12 16:32:39 +0200

    Fixed bugs

--- importer.sh
+++ importer.sh
@@ -98,7 +98,7 @@ selfUpdate() {
     fi
 }
 
-selfUpdate
+#selfUpdate
 
 getStatus() {
     if [[ $? != 0 ]];then
@@ -111,10 +111,9 @@ getStatus() {
 sshKeygen() {
     echo $awsKey | tr " " "\n" | base64 --decode > /tmp/$tmpKeyName
     chmod 600 /tmp/$tmpKeyName
-    agentPID=$(eval `ssh-agent` | awk {'print $3'})
+    eval `ssh-agent`
     ssh-add /tmp/$tmpKeyName
     getStatus "Generation ssh key"
-
     if [[ -z $PORT ]];then
         PORT="22"
     fi
@@ -153,19 +152,19 @@ mediaGet() {
 
 sqlExport() {
     if [[ $cms == "m1" ]];then
-        sqlDataInfo=`ssh $USER@$HOST -p $PORT -i /tmp/$tmpKeyName "cat $configPath" | grep "host\|username\|password\|dbname" | awk -F"[" {'print $3'} | awk -F"]" {'print $1'} | | head -4 | tr "\n" " "`
+        sqlDataInfo=`ssh $USER@$HOST -p $PORT -i /tmp/$tmpKeyName "cat $configPath" | grep -A 10 "<default_setup>" | grep "host\|username\|password\|dbname" | awk -F"[" {'print $3'} | awk -F"]" {'print $1'} | head -4 | tr "\n" " "`
         dbHost=`echo $sqlDataInfo | awk {'print $1'}`
         dbName=`echo $sqlDataInfo | awk {'print $4'}`
         dbUser=`echo $sqlDataInfo | awk {'print $2'}`
         userPass=`echo $sqlDataInfo | awk {'print $3'}`
     elif [[ $cms == "m2" ]];then
-        sqlDataInfo=`ssh $USER@$HOST -p $PORT -i /tmp/$tmpKeyName "cat $configPath" | grep "host\|username\|password\|dbname" | awk -F"=>" {'print $2'} | awk -F"'" {'print $2'} | head -4 | tr "\n" " "`
+        sqlDataInfo=`ssh $USER@$HOST -p $PORT -i /tmp/$tmpKeyName "cat $configPath" | grep -A 10 "'default' =>" | grep "host\|username\|password\|dbname" | awk -F"=>" {'print $2'} | awk -F"'" {'print $2'} | head -4 | tr "\n" " "`
         dbHost=`echo $sqlDataInfo | awk {'print $1'}`
         dbName=`echo $sqlDataInfo | awk {'print $2'}`
         dbUser=`echo $sqlDataInfo | awk {'print $3'}`
         userPass=`echo $sqlDataInfo | awk {'print $4'}`
     fi
-    ssh $USER@$HOST -p $PORT -i /tmp/$tmpKeyName "cd ~;mysqldump -h '$mysqlHost' '$dbName' -u'$dbUser' -p'$userPass' -v --routines --skip-triggers --single-transaction | gzip -9" > auto_$dbName.sql.gz && gzip -d auto_$dbName.sql.gz
+    ssh $USER@$HOST -p $PORT -i /tmp/$tmpKeyName "cd ~;mysqldump -h '$dbHost' '$dbName' -u'$dbUser' -p'$userPass' -v --routines --skip-triggers --single-transaction | gzip -9" > auto_$dbName.sql.gz && gzip -d auto_$dbName.sql.gz
     getStatus "SQL download"
 }
 sshCopyId() {
@@ -189,7 +188,7 @@ sshCopyId() {
 
 clearData() {
     rm -f /tmp/$tmpKeyName
-    kill $agentPID
+    ssh-agent -k
 }
 
 case $1 in
