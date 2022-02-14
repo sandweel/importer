7dbb328cdc8c0ac6982ddc1a3dccd77a4d42f4ab
Vitaliy <kalyuzhnyak@gmail.com>
2022-02-14 15:48:54 +0200

    Changed strategy to parse magento config

--- importer.sh
+++ importer.sh
@@ -1,64 +1,5 @@
 #!/usr/bin/env bash
 
-awsKey="
-LS0tLS1CRUdJTiBSU0EgUFJJVkFURSBLRVktLS0tLQpNSUlKS1FJQkFBS0NBZ0VBcjdDNTBMYUdP
-aEE1NHF6dXFsUEF4ZVliSkxXT1BDVnUzRGtIRWZndktiZXFETUJUCkJKa1AyNHgvQ2VQWFQyS2FO
-OHFJdTVPL1VKMyt3Y1lLUlg2eWtoc2IzZTUxMTJ4Wk5hbnlSZFpVZ0ZvaGtwUkYKeGV1bFkrYkxL
-KytFcDZHeDdoUXdDSWJwZW95QllpT1J0ZndOVG1PZjJ5MVZCRmo2QzBTMUdjbTZHU09tUUpudgpN
-UGdSeUZVZlY1UkFiZjROQVJrRnpEdFlVZXFkVzluRDIwQlJSMHRhYzZ0YzZjWkp0TVNYNkJvYk92
-TWNSOVB4Ck95R0JhRzI3Qy9STXBhSlAzdVVQbjdlcVVjcngvSlRWdVJDemxvY0RaYmc3b3QvbVNZ
-VURMbkkyRlErZE9Jd2sKUTFNRjZtbmdKK2RaWkxZdGVHUkhmVCtFRmRCeWJQQWprSmNDQ0tucDZW
-RnZBdVNtVGRGV3Fya2lhbzJDSGsvbApQK0pQREFlVXU2T1pCc29xSzJqV0MydXU1bnF4OUhtZy9K
-NXNaUFRMbzRXajNsNkNFRUxXQzVLNVVOdnB0enU2CnJGVk5kNDFBQmZ2M2psbXhjcmc2M05YT2lQ
-a081bDNLbHpjeUNMSTNESkgwUkFJQ0U4NVludmc0L1RmZkhkb3cKNlRIQnUyL3N6enMxWkJKdFpM
-eFU4RFAwOUcwQ2FYSmJLNDlmNHhnbzdVSFJpTU5DQnl6RHIwemJmRVZDeFMwQQpNU1JXbCtOam56
-L0lpaHQzRFFjZmc3ZW1EclB1d1J2YkY1MWFLUnczSjlDb0RPQ3krbU9rc0ZSSUZBMlRoS2pJCmZ2
-bSt3OWpqbDhSTlhkSkZoRjEwNXVSK1kwWDdocERpdEJvYlZOeTZibkhpMDNDQzBCUzM1ejNBeE5z
-Q0F3RUEKQVFLQ0FnQjJYZ0N5eDJaamxDOExzeXNSQW1Cc2RIMXhIQWo2S05ZcDlSbjRWMWVmS2Fp
-ZDhxMTcrWXVmaFdiSgp5UUYwOEorcVd3QUlNS0RDdGMrL3lhZDk3c0JzZzJzZytObllDNTJHOURy
-UUpuakVDUFY1aGdBRU9xRXhUM3N1CjNLNXBLaDF3bzVWQkNYUWdKcTI4NnUzdXlDUkczZ1FEYk9W
-TWlvM2NMYXc5OVpVMS9yaXg2WVFuWDlyTE45NW4KNlBBSHF4UWoyekgyeVR6VjQ5V2szbkNBTlRN
-TmI0MC9SUUxTVmMrdXhJejd5U3J6YW50eFFTL1RnTjFvK0JULwpGdGJVK1NnS3FoVCtySVZKbWd1
-eStxdUxoZDhZMlZuR25IdEJYc3dPV0ZrdVg3QmtQQ0xXeUVSZVAzOUswWTQrCkZEWThEaWxRWlc5
-c3lQQTZxamVOZ2JUVVk0andJdkExeTNITGdGZGhWREY5ZDJtbjVFc0FOVXlCRTVHRitXUEsKS0Mv
-SDM0cEovRzJEeVc0M0h3OWVtOENzVStnZU0rRytKZWwzajM3NjJSM3BkNXRVT3ZoTlAzQW5pU1Fs
-RWF6WQpxOE5ZYlVPSExPeU9QVFhscGliaUMraGVpZWZYL2Vzdm1hcEwvcWgrb0ZBb2MwYk4vR01E
-eGtzVCszZGNrdG1ZCmNCVlI0ZnRmQk1YTzZRSmw5SDNwMW1qU29hT3hvUHQ0eFJnT3NtM2czcDZO
-R2pZR1pCZTRoZ1VCTkx6RzFZWEwKTk1kZWJkaUVMZGdPT085YzFtUUdtMW5acWpCV1JrU2w1R2Ez
-czBFMGRBZDFKdHgyUTlFSXJTYitsdWRwbElhOQpWTXRFRmNuckJPd3lUbWhvbDQ0Zy8zanF4WklX
-Y1o4dTQxOXFyNHJhVFN2SGVydnRzUUtDQVFFQTRzL2xNcFIxCjFyTkZ2T3NiOHU3K2JkMktrSWFH
-bVEyOFFESWYrSlhDcHpqQnkzMHRiUjNCbklac25QeGRqeFd1ZThzZ1BXSXEKSEZUL3VKUTI5blFo
-Q0EydkhrdFFpVUczM1pOekdKVnZPbnE3ZVRlZmVpSy9ZaXNrM1ZOakhrMitYQ3duKysvaQovMHp3
-dXZtb3p4SWVKSDREbkpZczNkZkdTQ09Jc29xNjVwVEllRzBaR0NwKzBPMm0wQ0tCdEdXNjBrVGRD
-cWhqCllldzFySzV4ZTM0ZlRvbDRMNHM1cWlaUTVsNGcvMXR0K25YRjF0cHNnSW9OVE5Xc3BMUVVT
-RklLOTRETkJIWmsKY2V6c0c1dDBLa2lzUjVybTRKeDB4T3pJUHNOZHFKY3RqRUxCbWZhSUJxK1E5
-eHk0QlhPS2VKam5tVnlUcGN0TwpzTDdHa0NLZXBFcXFKUUtDQVFFQXhreXNma1FpL0xPSTlTMG10
-VjV2Wk5kZ0Zpd0FiUU5WdU9GWFNiQ1RpV2NDCkVsL25rU2twVmlBaVRLK0ZXVVZVZi91a2RiUG5C
-T3lMTUNSV3pJenJWRjlYVGRiWVl0ekkzd0l4NjdoMmIwSjQKckRZdlRpdzZtZ0YvZFpNbGtPM1Zs
-Ri9uZzhvRTlTOGQvZlhwTmlldjkzeFVGWnNNRVp6d01QSW5ueEl6SVZzMQpObllaWDZvNnFjYkcy
-a2g1WHVsOG9OMXVseGx5VUwwRFpPSDBlRmxWL25nYkQyWDNYT0xhMGpxNXBGVUV1THJ6CkZSQ1lv
-VWhpUTRGcHVLR1FHc3cyRmM1d0xKWU51TlNsSExRQ3cxcWxZVXo1Qnl4K1hLRG9IQ0xLWE1paDRm
-OUgKQWpWSmJ4WldwcUoxaktRRHg0bms5OUtCV0J2dnF3N3lZbG9ZVDhrQy93S0NBUUJqV0xDSjRT
-RmtjNDNlWEJVWAo2YnNrSUYvclFpRit0TmQ2UHZwbUJpTUc0QU55d0ZlL2JjTTZibDd3dkZHMnRN
-YUNqc3lHVnBNZmFkUEE0NXJYCmhMSmJOck0zejhGcy9ZNHR6Y0lpU0lJNTJUeDRzZloycHZXeGFE
-aHpnV3p1L3hxY2dQTGFsWTZXU1B0QXlzNDQKTlRWaXdwZU9aUEV1RXE5Vk5jdFFlUmU0bS83YlVO
-NG5DYVlXbWFIeGxNdS9XcmZrRmluRDIxSDh1RndpVVI1LworeFIzY0J0bXA5TVFSa3cxTzVHMGlE
-VzUrUkE1TmQ5RXhqTmhMazY2cmU0Y1oyTktXZTV6UC9CclBsMkZFaWhsCnhRTkJNZ1ppNStUMXhN
-bEpSSVduck9CVWlZRUhrVWF2dHdNR1dsdjRkMDExMlhQSVp4WjlEbnBmVEpEbzNXRWoKa1ZqSkFv
-SUJBUUNLN1k3d0M1YWppcmtLWks2NDVNZVFVK0JxeURzb0pPejYwVTFSUUZldExURGpaTE9kSE1S
-cwpKYWxDSnJwN0Y3cWl5OCtsOHZ1SWxKelRocklWTHlsMGZETnpRbkZwbWh0Vk9ubDFGMUJLbEx4
-c2txM0R5L0xtCmtPUVdvSmZ5R043SXpBUXNiZTNaUlVCeSs4Visvb0VuRjhEbFBqSlBhSzlTNjJU
-YzZtaE9GMU9KcTJZTmd2c0UKbFZJZC9pYWlXMXpJYXpxZXUrSStOSjViSUg4aGpoMVVZaHM3UjRu
-dFZyWk9FWHljYUVBZG1FVFQza1BBd3pDcwo4TTc3M3AxU1dQNFBld2xXQTI3cnJuZGJTN2FmM1RM
-K24ybmRIOHdMNDJPYXlya1NGakpzWG1nR1lRVG93akxGCkhnN1RFTm5oQTNnYU5DNFFaOG9iQXli
-SHh5MC9RS3BMQW9JQkFRQ1pDWlZUZ01Oc0NMWlJmS3EyQWdoYUt1OFcKZW8veHZ2U1U3VjFJNXRH
-QU9ocUV3YndmODJ1bndLVXpUU3Z6bFd5ODVZZmtLQTI1QzI0c1Y3dXBtUXF3aUNNQwpORERSVXZJ
-dGpOTnJmVXVqeWgxRCtlSitJSVBJRGkrZ2pHYU1EUDgzMkdkZ1JaS3dUK3VaYnVSczVEZWtCV3J1
-CkNVcFVpYnJrWkZPOFhhdklxeFRCRGJkQmZJWDFYdG1vZ0cwajNvMVRHckxhZC9xelA2WmJKaFVG
-TmNlNHFpa24KVHpvaGNZUnNCL0xSd2M1bWxvN05vdU1NTlpXMUFtV3NZaE00SUtxdFE3REw5VmhU
-V3I2bkoxSDdNRVFBZU1kYwp6bnVsWkFROVN0SC9QM1JzSTdZY1RLRTllc29uZjZuT2Rua2FXQ0FH
-QnlDeHhlNCttbGx3UHRQcmg1MmoKLS0tLS1FTkQgUlNBIFBSSVZBVEUgS0VZLS0tLS0K
-"
 tmpKeyName="aws_insecure_key"
 HOST="$2"
 PORT="$3"
@@ -177,10 +118,7 @@ selfUpdate
 prepare
 
 
-
-
 sshKeygen() {
-    #echo $awsKey | tr " " "\n" | base64 --decode > /tmp/$tmpKeyName
     if [[ -z $PORT ]];then
         PORT="22"
     fi
@@ -218,7 +156,7 @@ cmsDetector() {
     if [[ -z $cmsPath ]];then
         cmsPath="~/public_html"
     fi
-
+    phpTmpFile="/tmp/.importer_config_parse.sh"
     cConf=`$sshExec "stat $cmsPath/app/etc/local.xml &>/dev/null && echo 1 && exit 0 || stat $cmsPath/app/etc/env.php &>/dev/null && echo 2 && exit 0"`
     if [[ $cConf == "1" ]];then
         cms="m1"
@@ -268,15 +206,24 @@ EOF
         dbUser=$(echo ${sqlDataInfo[dbuser]})
         userPass=$(echo ${sqlDataInfo[dbpass]})
     elif [[ $cms == "m2" ]];then
-        declare -A sqlDataInfo=$($sshExec "cd $cmsPath; php -r \"\\\$a = require('$configPathFile'); print '( [dbhost]='.\\\$a['db']['connection']['default']['host']; print ' [dbname]='.\\\$a['db']['connection']['default']['dbname']; print ' [dbuser]='.\\\$a['db']['connection']['default']['username']; print ' [dbpass]='.\\\$a['db']['connection']['default']['password'] . ' )';\"")
-        echo <<EOF 1>/dev/null
-Fixing MC syntax lighting"
-EOF
+        echo "<?php" > $phpTmpFile
+        echo "\$a = require('$configPathFile');" >> $phpTmpFile
+        echo "print('([dbhost]='.\$a['db']['connection']['default']['host']);" >> $phpTmpFile
+        echo "print(' [dbname]='.\$a['db']['connection']['default']['dbname']);" >> $phpTmpFile
+        echo "print(' [dbuser]='.\$a['db']['connection']['default']['username']);" >> $phpTmpFile
+        echo "print(' [dbpass]='.\$a['db']['connection']['default']['password'].')');" >> $phpTmpFile
+
+        if [[ -z $PASSWORD ]];then
+            rsync -e "ssh -o PasswordAuthentication=no -o StrictHostKeyChecking=no -p $PORT -i /tmp/$tmpKeyName" $phpTmpFile $USER@$HOST:$phpTmpFile
+        else
+            sshpass -p$PASSWORD rsync -e "ssh -o PasswordAuthentication=no -o StrictHostKeyChecking=no -p $PORT" $phpTmpFile $USER@$HOST:$phpTmpFile
+        fi
+
+        declare -A sqlDataInfo=$($sshExec "cd $cmsPath; php $phpTmpFile")
         dbHost=$(echo ${sqlDataInfo[dbhost]})
         dbName=$(echo ${sqlDataInfo[dbname]})
         dbUser=$(echo ${sqlDataInfo[dbuser]})
         userPass=$(echo ${sqlDataInfo[dbpass]})
-
     fi
 
     if [[ ! -z $userPass ]];then
